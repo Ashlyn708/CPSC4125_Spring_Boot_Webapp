58d885aa7a0fd37d7f6b9690d7886e699f69fa61
Ashlyn708 <averett_ashlyn@columbusstate.edu>
2020-09-08 23:17:51 +0000

    java class for contacts

---
+++ complete/src/main/java/com/example/springboot/Contact.java
@@ -0,0 +1,21 @@
+package com.example.springboot;
+public class Contact {
+    private String email;
+    private String message;
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String newEmail){
+        this.email=newEmail;
+    }
+
+    public String getmessage(){
+        return message;
+    }
+
+    public void setMessage(String newMessage){
+        this.message=newMessage;
+    }
+}
