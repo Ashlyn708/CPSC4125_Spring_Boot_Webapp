9c31fb0f456301580c1421617e1c9459b745f79e
Ashlyn708 <averett_ashlyn@columbusstate.edu>
2020-09-10 22:14:11 +0000

    fixed contact java

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import com.example.springboot.Contact;
 
 Press Shift + Tab to navigate to chat history.
 
@@ -22,16 +21,21 @@ public class HelloController {
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
 		return "greeting";
-	}
+    }
+    
     @GetMapping("/contact")
 	public String contact( Model model) {
 		return "contact";
     }
-    @GetMapping("/contact")
-    public String postContact(Model,model,@RequestBody Contact contact){
-        System.out.printIn(contact.getEmail());
-        return "contact"
+
+    @PostMapping("/contact")
+    public String postContact(Model model, @RequestBody Contact contact){
+        //Contact create object
+        System.out.println(contact.getEmail());
+        System.out.println(contact.getMessage());
+        return "contact";
     }
+
     @GetMapping("/about")
 	public String about( Model model) {
 		return "about";
